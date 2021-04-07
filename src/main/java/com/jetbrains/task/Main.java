e4512b964eed638e58011c6e4581a18cd2eb1dad
Readrid <890readrid@gmail.com>
2021-04-07 18:31:32 +0300

    Fix CLI processing

--- src/main/java/com/jetbrains/task/Main.java
+++ src/main/java/com/jetbrains/task/Main.java
@@ -8,16 +8,20 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        String input;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        input = reader.readLine();
-        while (!input.equals("exit")) {
+    public static void main(String[] args) {
+        try {
+            String input;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             input = reader.readLine();
-            Parser parser = new Parser(input);
-            if (parser.parse()) {
-                System.out.println(parser.getSimplifiedLine());
+            while (!input.equals("exit")) {
+                Parser parser = new Parser(input);
+                if (parser.parse()) {
+                    System.out.println(parser.getSimplifiedLine());
+                }
+                input = reader.readLine();
             }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
